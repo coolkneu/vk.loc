49a9aa69513fba35ff1e43e0e03ff391938e8719
Artem <cool.kneu@mail.ru>
2014-08-16 09:39:56 +0300

    Добавление всех файлов

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Register</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=windws-1251" />
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="robots" content="index,follow" />
+	<link rel="stylesheet" type="text/css" href="register.css" />
+</head>
+
+<body>
+	<h1>Регистрация</h1>
+	<div class="Authorization">
+		<div class="abody">
+			<a href="http://mysite.ua/registration.php" class="lost">Напомнить пароль?</a>
+			<p class="login">ВОЙТИ</p>
+			  <form method="POST" align="" class="field">
+					<input type="text" name="Login" placeholder="Логин"/>
+					<input type="text" name="Email" placeholder="Email"/>
+					<input type="password" password="password" placeholder="Пароль"/>
+			  </form>	
+			  <p><input type="checkbox" name="remember" class="remember">Запомнить пароль</p>
+		</div>
+		<div class="afooter">
+			<button type="submit" onclick="">ВОЙТИ</button>
+			<a href="/vk.loc.html" class="akaunt">Новый аккаунт</a>
+		</div>
+</body>
+</html>
